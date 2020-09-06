6ccbe5cd6f7d138cf8f0e49e6ad60bcf9a2b89e8
mbennis01 <38289647+mbennis01@users.noreply.github.com>
2020-09-06 18:53:50 +0200

    Update Option3.java

--- app/src/main/java/com/anistream/xyz/scrapers/Option3.java
+++ app/src/main/java/com/anistream/xyz/scrapers/Option3.java
@@ -38,7 +38,7 @@ public class Option3 extends Scraper {
 
         ArrayList<Quality> qualities = new ArrayList<>();
         try {
-            String vidStreamUrl = "https:" + gogoAnimePageDocument.getElementsByClass("play-video").get(0).getElementsByTag("iframe").get(0).attr("src");
+            String vidStreamUrl = gogoAnimePageDocument.getElementsByClass("play-video").get(0).getElementsByTag("iframe").get(0).attr("src");
             String m3u8Link = "";
             String htmlToParse = "";
             String vidCdnUrl = vidStreamUrl;
